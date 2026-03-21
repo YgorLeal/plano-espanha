14af6d1e248f5dd13cb9bcc5a305c8152f7e125a
Ygor Leal <ygorleal682@gmail.com>
2026-03-21 05:02:38 +0100

    Add Google Tag Manager snippet

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -43,7 +43,26 @@ export default function LangLayout({
 }) {
   return (
     <html lang={params.lang}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7T3ZKQ5');`,
+          }}
+        />
+      </head>
       <body className="min-h-screen flex flex-col">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7T3ZKQ5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: toJsonLd(organizationSchema) }}
